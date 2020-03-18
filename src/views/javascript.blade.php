8163bcadc1b91f46d40219e13505641fef854057
PovilasKorop <povilask007@gmail.com>
2020-03-18 10:45:48 +0200

    Conditions option, Multiple Datasets and Colors

--- src/views/javascript.blade.php
+++ src/views/javascript.blade.php
@@ -1,40 +1,50 @@
 <script>
-  var ctx = document.getElementById("{{ $options['chart_name'] ?? 'myChart' }}");
-  var {{ $options['chart_name'] ?? 'myChart' }} = new Chart(ctx, {
-    type: '{{ $options['chart_type'] ?? 'line' }}',
-    data: {
-      labels: [
-          @foreach ($data as $group => $result)
-            "{{ $group }}",
-          @endforeach
-      ],
-
-      datasets: [{
-        label: '{{ $options['chart_title'] }}',
-        data: [
-            @foreach ($data as $group => $result)
-            {!! $result !!},
+    var ctx = document.getElementById("{{ $options['chart_name'] ?? 'myChart' }}");
+    var {{ $options['chart_name'] ?? 'myChart' }} = new Chart(ctx, {
+        type: '{{ $options['chart_type'] ?? 'line' }}',
+        data: {
+            labels: [
+                @foreach ($datasets[0]['data'] as $group => $result)
+                    "{{ $group }}",
+                @endforeach
+            ],
+        datasets: [
+            @foreach ($datasets as $dataset)
+            {
+                label: '{{ $dataset['name'] ?? $options['chart_name'] }}',
+                data: [
+                    @foreach ($dataset['data'] as $group => $result)
+                        {!! $result !!},
+                    @endforeach
+                ],
+                @if ($options['chart_type'] == 'line')
+                    fill: false,
+                    @if (isset($dataset['color']) && $dataset['color'] != '')
+                        borderColor: '{{ $dataset['color'] }}',
+                    @else
+                        borderColor: 'rgba({{ rand(0,255) }}, {{ rand(0,255) }}, {{ rand(0,255) }}, 0.2)',
+                    @endif
+                @elseif ($options['chart_type'] == 'pie')
+                    backgroundColor: [
+                        @foreach ($dataset['data'] as $group => $result)
+                            'rgba({{ rand(0,255) }}, {{ rand(0,255) }}, {{ rand(0,255) }}, 0.2)',
+                        @endforeach
+                    ],
+                @endif
+                borderWidth: 3
+            },
             @endforeach
-        ],
-        @if ($options['chart_type'] == 'pie')
-        backgroundColor: [
-            @foreach ($data as $group => $result)
-                'rgba({{ rand(0,255) }}, {{ rand(0,255) }}, {{ rand(0,255) }}, 0.2)',
-            @endforeach
-        ],
-        @endif
-        borderWidth: 1
-      }]
+        ]
     },
     options: {
-      scales: {
-        xAxes: [],
-        yAxes: [{
-          ticks: {
-            beginAtZero:true
-          }
-        }]
-      }
+        scales: {
+            xAxes: [],
+            yAxes: [{
+                ticks: {
+                    beginAtZero:true
+                }
+            }]
+        }
     }
-  });
+    });
 </script>
