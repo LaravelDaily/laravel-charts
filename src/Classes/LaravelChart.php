ae93c7dc1fca92c3cd14f41884aa5698bdc1cdda
Mitul Koradiya <mitulkoradiya18@gmail.com>
2022-01-13 12:39:14 +0530

    Add function to return datasets
    Add function to return datasets for the next ajax request to update data in the graph.

--- src/Classes/LaravelChart.php
+++ src/Classes/LaravelChart.php
@@ -295,4 +295,11 @@ class LaravelChart
     {
         return '<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.5.0/Chart.min.js"></script>';
     }
+    
+    /**
+     * @return array
+     */
+    public function getDatasets() {
+        return $this->datasets;
+    }
 }
